73793ed5c3b9d881bed00d4cdc43619a97867a99
Mariana <marihpimenta@hotmail.com>
2024-09-26 11:14:28 +0100

    Update 3_visao_restaurantes.py

--- pages/3_visao_restaurantes.py
+++ pages/3_visao_restaurantes.py
@@ -16,7 +16,7 @@ from streamlit_folium import folium_static
 
 st.set_page_config( page_title='Visão Restaurantes', page_icon='🍽️', layout='wide' )
 
-df = pd.read_csv( '../dataset/train.csv' )
+df = pd.read_csv( 'dataset/train.csv' )
 
 df1 = df.copy()
 
